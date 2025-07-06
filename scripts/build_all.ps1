230e3291a701e59e395e39cc14fdbbfd2fe3c2d2
Marc-Antoine Lortie <malortie@users.noreply.github.com>
2025-07-06 13:11:33 -0400

    Update build_all.ps1

--- scripts/build_all.ps1
+++ scripts/build_all.ps1
@@ -1,12 +1,10 @@
 & "$PSScriptRoot\..\gitmodules\mod-installer\build_all.ps1" `
-  -Version "<version>" `
+  -Version "2.0.0" `
   -PreReleaseVersion "beta.1" `
-  -OutputFilePrefix "<output file prefix>" `
-  -ModName "<mod name>" `
-  -ModFolder "<mod folder>" `
-  -AdditionalModFolders @() `
+  -OutputFilePrefix "Halloween_2005" `
+  -ModName "Halloween HL2: Pumpkin Night (2005)" `
+  -ModFolder "Halloween" `
   -ModFilesPath "$PSScriptRoot\..\game" `
-  -ModBaseFilesUrl "<url to mod base files zip>" `
-  -ModBaseFilesUrlHash "<mod base files zip sha256>" `
-  -ModReadmePath "<mod readme path>" `
+  -ModBaseFilesUrl "https://github.com/HL2-Mods-Legacy-Project/hl2-legacy-registry/releases/download/halloween2005/Halloween-2005-Original.zip" `
+  -ModBaseFilesUrlHash "0140c2b44b2bbc5b43c43bbb702983ca3f2b9e643fb3be0d5b5642fea22a1adf" `
   -PublishPath "$PSScriptRoot\..\artifacts\publish"
